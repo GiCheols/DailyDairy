37529364ca01ef0174c9977e0124cf83fd1fb5a9
GiCheols <ngc4665@naver.com>
2024-06-04 00:57:17 +0900

    Initial Commit

---
+++ 2171278TermProject/2171278TermProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2171278TermProject
+//
+//  Created by 남기철 on 2024/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
