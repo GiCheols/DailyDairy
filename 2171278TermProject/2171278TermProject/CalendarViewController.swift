ab967d6d62b40670b7d72330128781c17ff8fd55
GiCheols <ngc4665@naver.com>
2024-06-05 22:44:40 +0900

    달력 UI 기본 기능 추가

--- 2171278TermProject/2171278TermProject/CalendarViewController.swift
+++ 2171278TermProject/2171278TermProject/CalendarViewController.swift
@@ -1,21 +1,82 @@
-//
-//  ViewController.swift
-//  2171278TermProject
-//
-//  Created by 남기철 on 2024/06/04.
-//
-
 import UIKit
 import FSCalendar
 
 class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDelegateAppearance, FSCalendarDataSource {
+    
+    @IBOutlet weak var beforeDate: UIButton!
+    @IBOutlet weak var nextDate: UIButton!
+    @IBOutlet weak var changeScopeButton: UIButton!
     @IBOutlet weak var calendarHeight: NSLayoutConstraint!
     @IBOutlet weak var mainCalendar: FSCalendar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setCalendar(calendar: mainCalendar)
     }
 
-
+    func setCalendar(calendar : FSCalendar) {
+        mainCalendar.delegate = self
+        mainCalendar.dataSource = self
+        
+        mainCalendar.scrollEnabled = true
+        mainCalendar.scrollDirection = .horizontal
+        
+        mainCalendar.backgroundColor = .white
+        
+        mainCalendar.appearance.headerTitleFont = .systemFont(ofSize: 15.0)
+        mainCalendar.appearance.headerDateFormat = "yyyy년 MM월"
+        mainCalendar.calendarHeaderView.layoutMargins = .init(top: 10, left: 10, bottom: 10, right: 10)
+        
+        mainCalendar.appearance.weekdayFont = .systemFont(ofSize: 15.0)
+    }
+    
+    @IBAction func clickChangeScope(_ sender: UIButton) {
+        if self.mainCalendar.scope == .month {
+            self.changeCalendarScope(calendar: self.mainCalendar, month: false)
+            self.changeScopeButton.setTitle("월간 달력 보기", for: .normal)
+            self.beforeDate.setTitle("저번 주", for: .normal)
+            self.nextDate.setTitle("다음 주", for: .normal)
+            
+        } else {
+            self.changeCalendarScope(calendar: self.mainCalendar, month: true)
+            self.changeScopeButton.setTitle("주간 달력 보기", for: .normal)
+            self.beforeDate.setTitle("저번 달", for: .normal)
+            self.nextDate.setTitle("다음 달", for: .normal)
+        }
+    }
+    
+    func changeCalendarScope(calendar: FSCalendar, month: Bool) {
+        calendar.setScope(month ? .month : .week, animated: true)
+    }
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeight.constant = bounds.height
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+            calendar.reloadData()
+        }
+    }
+    
+    @IBAction func beforeDate(_ sender: UIButton) {
+        self.actionMoveDate(calendar: self.mainCalendar, moveUp: false)
+    }
+    
+    @IBAction func afterDate(_ sender: UIButton) {
+        self.actionMoveDate(calendar: self.mainCalendar, moveUp: true)
+    }
+    
+    func actionMoveDate(calendar: FSCalendar, moveUp: Bool) {
+        let moveDirection = moveUp ? 1 : -1
+        
+        if calendar.scope.rawValue == 0 {
+            if let newDate = Calendar.current.date(byAdding: .month, value: moveDirection, to: calendar.currentPage) {
+                calendar.setCurrentPage(newDate, animated: true)
+            }
+        } else {
+            if let newDate = Calendar.current.date(byAdding: .weekOfMonth, value: moveDirection, to: calendar.currentPage) {
+                calendar.setCurrentPage(newDate, animated: true)
+            }
+        }
+    }
 }
 
